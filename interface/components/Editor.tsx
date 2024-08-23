229c2aab5362dde0feeb38a8f9d59d7d1e347b69
Luke M <44519853+lkdm@users.noreply.github.com>
2024-08-24 09:00:28 +1000

    feat: memoise the editor component.

--- interface/components/Editor.tsx
+++ interface/components/Editor.tsx
@@ -6,11 +6,14 @@ interface EditorProps {
   handleSetChunk: (index: number, content: string) => void,
 }
 
+// Editor should only re-render if the focusedNode changes
+const MemoizedInnerEditor = React.memo(InnerEditor);
+
 const Editor = ({chunks, handleSetChunk}: EditorProps) => {
   const [focusedNode, setFocusedNode] = React.useState<number | undefined>(undefined)
   return chunks.map((chunk, index: number) => (
     (focusedNode === index)
-    ? <InnerEditor
+    ? <MemoizedInnerEditor
         key={index}
         defaultContent={chunk}
         setContent={(content: string) => handleSetChunk(index, content)}

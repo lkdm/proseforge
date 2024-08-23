25018a791a1557a52c7682b86d9c576df7518676
Luke M <44519853+lkdm@users.noreply.github.com>
2024-08-23 22:12:32 +1000

    feat: add plugins to markdown editor.

--- packages/markdown-editor/components/Editor.tsx
+++ packages/markdown-editor/components/Editor.tsx
@@ -3,6 +3,9 @@ import { defaultValueCtx, Editor, rootCtx } from '@milkdown/kit/core';
 import { nord } from '@milkdown/theme-nord';
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
 import { listener, listenerCtx } from "@milkdown/plugin-listener";
+import { history } from '@milkdown/plugin-history';
+import { clipboard } from '@milkdown/kit/plugin/clipboard';
+import { indent } from '@milkdown/kit/plugin/indent';
 import { commonmark } from '@milkdown/kit/preset/commonmark';
 
 // TOOD: https://codesandbox.io/p/sandbox/react-grdxqn?file=%2Fsrc%2FApp.js%3A98%2C9-98%2C23
@@ -36,6 +39,9 @@ const MilkdownEditor = ({defaultContent, setContent}: EditorProps) => {
               })
         })
         .use(commonmark)
+        .use(history)
+        .use(clipboard)
+        .use(indent)
         .use(listener)
   );
 

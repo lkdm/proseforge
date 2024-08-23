2ca7e93587dd2019336e8d31c21c0d59252eaf6a
Luke M <44519853+lkdm@users.noreply.github.com>
2024-08-24 08:54:10 +1000

    feat: only load editor on active node.

--- interface/components/Editor.tsx
+++ interface/components/Editor.tsx
@@ -7,12 +7,16 @@ interface EditorProps {
 }
 
 const Editor = ({chunks, handleSetChunk}: EditorProps) => {
-  return chunks.map((chunk, index) => (
-    <InnerEditor
-      key={index}
-      defaultContent={chunk}
-      setContent={(content: string) => handleSetChunk(index, content)}
-    />
+  const [focusedNode, setFocusedNode] = React.useState<number | undefined>(undefined)
+  return chunks.map((chunk, index: number) => (
+    (focusedNode === index)
+    ? <InnerEditor
+        key={index}
+        defaultContent={chunk}
+        setContent={(content: string) => handleSetChunk(index, content)}
+      />
+      : <div onClick={() => setFocusedNode(index)} key={index}>{chunk}</div>
+
   ))
 }
 

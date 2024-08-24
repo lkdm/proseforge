06268ebbec839f4387244dd68c57017f95ba9842
Luke M <44519853+lkdm@users.noreply.github.com>
2024-08-24 11:32:39 +1000

    feat: more prose css tweaks.

--- interface/app/Layout.tsx
+++ interface/app/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => (
-  <main className="flex flex-col">
+  <main className="flex flex-col h-screen">
     <div className="flex flex-1 overflow-hidden">{children}</div>
   </main>
 );

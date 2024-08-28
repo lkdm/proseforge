b0c432c781b1475b4f0698eabba615ac587a5e1a
Luke M <44519853+lkdm@users.noreply.github.com>
2024-08-28 17:46:07 +1000

    feat: make Node return repositories.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,16 +4,23 @@ pub mod node;
 use editor::ports::{
     component::ComponentRepository, content::ContentRepository, project::ProjectRepository,
 };
+use node::ports::NodeConfigRepository;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use thiserror::Error;
 
 #[derive(Debug, Clone)]
 /// The application state available to all request handlers.
-pub struct Node<PRR: ProjectRepository, CMP: ComponentRepository, COR: ContentRepository> {
+pub struct Node<
+    PRR: ProjectRepository,
+    CMP: ComponentRepository,
+    COR: ContentRepository,
+    CFG: NodeConfigRepository,
+> {
     pub document_repo: Arc<PRR>,
     pub component_repo: Arc<CMP>,
     pub content_repo: Arc<COR>,
+    pub config_repo: Arc<CFG>,
     // Todo: Implement config repo
 }
 
@@ -23,12 +30,25 @@ pub enum NodeError {
     RepositoryError,
 }
 
-impl<DR: DocumentRepository> Node<DR> {
-    pub fn new(document_repo: DR) -> Result<Self, NodeError> {
+impl<
+        P: ProjectRepository,
+        C: ComponentRepository,
+        CO: ContentRepository,
+        CFG: NodeConfigRepository,
+    > Node<P, C, CO, CFG>
+{
+    pub fn new(
+        project_repo: P,
+        component_repo: C,
+        content_repo: CO,
+        config_repo: CFG,
+    ) -> Result<Self, NodeError> {
         let node = Node {
-            document_repo: Arc::new(document_repo),
+            document_repo: Arc::new(project_repo),
+            component_repo: Arc::new(component_repo),
+            content_repo: Arc::new(content_repo),
+            config_repo: Arc::new(config_repo),
         };
-        // TODO: Return Arc of Node
         return Ok(node);
     }
 }

0e1dbc1399f13beaf5b7deab48a65f0d3ee54923
Luke M <44519853+lkdm@users.noreply.github.com>
2024-08-30 18:53:30 +1000

    wip

--- adapters/sqlite/src/lib.rs
+++ adapters/sqlite/src/lib.rs
@@ -1,95 +1,100 @@
-use std::sync::{Arc, Mutex};
-
-use pf_core::{
-    editor::{
-        models::{ContentId, CreateContentError, CreateContentRequest},
-        ports::ContentRepository,
+pub mod repositories;
+use chrono::NaiveDateTime;
+use proseforge_core::editor::{
+    models::{
+        Content, ContentId, CreateContentError, CreateContentRequest, GetContentError,
+        GetContentRequest,
     },
-    node::Id,
+    ports::ContentRepository,
 };
-use rusqlite::{params, Connection, Result};
+use sqlx::Row;
+use sqlx::{migrate::MigrateDatabase, Error as SqlxError, Sqlite, SqlitePool};
+use sqlx::{
+    query,
+    sqlite::{SqliteConnectOptions, SqlitePoolOptions},
+};
+use std::sync::{Arc, Mutex};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum SqliteAdapterError {
     #[error("Database error: {0}")]
-    DatabaseError(#[from] rusqlite::Error),
+    DatabaseError(#[from] SqlxError),
+    // Add more specific error types if needed
 }
 
 #[derive(Clone, Debug)]
 struct SqliteAdapter {
-    connection: Arc<Mutex<Connection>>,
+    pool: Arc<SqlitePool>,
 }
 
 impl SqliteAdapter {
-    pub fn new(database_url: &str) -> Result<Self> {
-        let connection = Connection::open(database_url)?;
-        connection.execute_batch(
-            "
-            CREATE TABLE IF NOT EXISTS project (
-                id ULID PRIMARY KEY DEFAULT ulid(),
-                title TEXT NOT NULL,
-                component_ids TEXT NOT NULL,
+    pub async fn new(database_url: &str) -> Result<Self, sqlx::Error> {
+        let pool = SqlitePool::connect(database_url).await?;
 
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                deleted_at TIMESTAMP,
-            );
-            CREATE TABLE IF NOT EXISTS content (
-                id ULID PRIMARY KEY DEFAULT ulid(),
-                data TEXT NOT NULL,
+        // Ensure the database is migrated to the latest version
+        sqlx::migrate!().run(&pool).await?;
 
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                deleted_at TIMESTAMP,
-            );
-            CREATE TABLE IF NOT EXISTS component (
-                id ULID PRIMARY KEY DEFAULT ulid(),
-                kind TEXT NOT NULL,
-                component_ids TEXT NOT NULL,
-                content_id ULID,
-                summary_id ULID,
-                document_id ULID,
+        Ok(SqliteAdapter {
+            pool: Arc::new(pool),
+        })
+    }
+}
 
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                modified_at TIMESTAMP,
-                deleted_at TIMESTAMP,
-            );
-        ",
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::{Context, Result};
+    use sqlx::{query, SqlitePool};
+    use tokio;
+
+    const DATABASE_URL: &str = "sqlite::memory:";
+
+    async fn setup() -> Result<SqlitePool, Box<dyn std::error::Error>> {
+        // Connect to the in-memory SQLite database
+        std::env::set_var("DATABASE_URL", "sqlite::memory:");
+        // let options = SqliteConnectOptions::default().create_if_missing(true);
+        // .extension("ulid0");
+        let pool = SqlitePool::connect(DATABASE_URL).await?;
+        // let pool: Pool<sqlx::Sqlite> = SqlitePool::connect_with(options).await?;
+
+        Ok(pool)
+    }
+
+    #[tokio::test]
+    async fn test_migrations() -> Result<(), Box<dyn std::error::Error>> {
+        let pool = setup().await?;
+        sqlx::migrate!()
+            .run(&pool)
+            .await
+            .context("Failed to apply migrations.");
+
+        let list_tables = query("SELECT name FROM sqlite_master WHERE type='table'")
+            .fetch_all(&pool)
+            .await?;
+        let tables_count = list_tables.len();
+        assert!(tables_count > 0);
+
+        let table =
+            sqlx::query("SELECT name FROM sqlite_master WHERE type='table' AND name='project'")
+                .fetch_optional(&pool)
+                .await;
+        let table_exists = table?.is_some();
+
+        assert!(table_exists, "Table 'project' should exist.");
+
+        // Check if a column exists in the table
+        let rows = query("PRAGMA table_info(project)").fetch_all(&pool).await?;
+        let column_exists = rows.iter().any(|row| {
+            let name: String = row.try_get("name").unwrap_or_default();
+            name == "id"
+        });
+
+        assert!(
+            column_exists,
+            "Column 'id' should exist in table 'project'."
         );
-        let connection = Arc::new(Mutex::new(connection));
-        Ok(Self { connection })
+
+        Ok(())
     }
 }
-
-// PRR: ProjectRepository,
-// CMP: ComponentRepository,
-// COR: ContentRepository,
-
-impl ContentRepository for SqliteAdapter {
-    async fn create_content(
-        &self,
-        _req: &CreateContentRequest,
-    ) -> Result<ContentId, CreateContentError> {
-        let conn = self.connection.lock().unwrap();
-        let id: Id = conn.query_row(
-            "
-            INSERT INTO content (data, created_at, modified_at)
-            VALUES (?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-            RETURNING id
-            ",
-            params![_req.data],
-            |row| row.get(0), // Extract the ID from the row
-        )?;
-        Ok(ContentId::new(id))
-    }
-}
-
-// CREATE TABLE IF NOT EXISTS content (
-//     id ULID PRIMARY KEY DEFAULT ulid(),
-//     data TEXT NOT NULL,
-
-//     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-//     modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//     deleted_at TIMESTAMP,
-// );

3f3ad6e8bf27c38812da5afa92e607425f5988cd
Luke Martin <44519853+lkdm@users.noreply.github.com>
2024-09-20 17:12:57 +1000

    feat: basic tree and structs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-type DocumentId = u32;
-
 #[derive(Debug, Clone)]
 struct Document {
-    id: DocumentId,
     content: String,
     is_modified: bool,
 }
@@ -17,31 +14,42 @@ enum DraftNode {
     Branch {
         title: String,
         summary: String,
-        children: Vec<DraftNode>,
     },
 }
 
 #[derive(Debug, Clone)]
 enum NoteNode {
-    Leaf {
-        title: String,
-        content: Document,
-    },
-    Branch {
-        title: String,
-        children: Vec<NodeNode>,
-    },
+    Leaf { title: String, content: Document },
+    Branch { title: String },
 }
 
-struct Draft {
-    children: Vec<DraftNode>,
+type NoteId = u32;
+type DraftId = u32;
+
+/// A project defines the
+struct Project {
+    draft: DirectoryTree<DraftId, DraftNode>,
+    notes: DirectoryTree<NoteId, NoteNode>,
 }
 
-// Use Node to generic enum.
-enum Node<T> {
-    Branch {
-        value: T,
-        children: Vec<Box<Node<T>>>,
-    },
-    Leaf(T),
+// Make a tree with indexmap as backing storage
+use indexmap::{map::*, Equivalent};
+
+/// DirectoryTree
+///
+/// A generic data structure for a tree with nodes that are ordered arbitrarily. This is perfect for
+/// a directory-file tree that allows the user to define an arbitrary order.
+///
+/// The key-value pairs have a consistent order that is determined by the sequence of insertion and removal calls on the map.
+///
+/// Index map is a hash table where the iteration order of key-value pairs is independent of the hash values of the keys.
+///
+/// Keys must impl Equivalent
+struct DirectoryTree<K: Equivalent<K>, V>(IndexMap<K, V>);
+
+impl<K: Equivalent<K>, V> DirectoryTree<K, V> {
+    /// Creates a new empty DirectoryTree.
+    pub fn new() -> Self {
+        DirectoryTree(IndexMap::new())
+    }
 }

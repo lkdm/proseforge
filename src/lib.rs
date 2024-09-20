41e2ff4cf3123e11aba4d2abe5849ae4ced179a2
Luke Martin <44519853+lkdm@users.noreply.github.com>
2024-09-21 09:38:31 +1000

    feat: add root children vector.

--- src/lib.rs
+++ src/lib.rs
@@ -10,28 +10,25 @@ use ulid::Ulid;
 use std::hash::Hash;
 
 #[derive(Clone)]
-enum Node<K, B, L> {
+enum Node<K, V> {
     /// Branch is analagous to a directory
-    Branch { key: K, value: B, children: Vec<K> },
+    Branch { value: V, children: Vec<K> },
     /// Leaf is analagous to a file
-    Leaf { key: K, value: L },
+    Leaf { value: V },
 }
 
 #[bon]
-impl<K, B, L> Node<K, B, L> {
+impl<K, V> Node<K, V> {
     /// Create a new Node
     ///
     /// Ideally you should use a bon builder for this
     #[builder]
-    fn new(key: K, branch: Option<B>, leaf: Option<L>, children: Option<Vec<K>>) -> Self {
-        match (branch, leaf) {
-            (Some(b), None) => Node::Branch {
-                key,
-                value: b,
-                children: children.unwrap_or_else(Vec::new),
-            },
-            (None, Some(l)) => Node::Leaf { key, value: l },
-            _ => panic!("A node must be either a Branch or a Leaf, not both or neither."),
+    fn new(value: V, children: Option<Vec<K>>) -> Self {
+        match children {
+            Some(children) => {
+                Self::Branch { value, children }
+            }
+            _ => Self::Leaf { value }
         }
     }
     fn push(&mut self, key: K) -> () {
@@ -66,21 +63,59 @@ impl<K, B, L> Node<K, B, L> {
 /// Represented as a flat list of nodes, by key, with references to their children.
 ///
 /// K: Key
-/// B: Branch
-/// L: Leaf
-pub(crate) struct Tree<K: Key, B, L>(SlotMap<K, Node<K, B, L>>);
-impl<K: Key, B: Clone, L: Clone> Tree<K, B, L> {
-    fn new() -> Self {
-        Tree(SlotMap::with_key())
+/// V: Node
+pub(crate) struct Tree<K: Key, V> {
+    /// SlotMap of key -> Node, with each Node containing a Vec of keys as its children
+    nodes: SlotMap<K, Node<K, V>>,
+    /// Reference children at the root.
+    children: Vec<K>
+}
+
+#[bon]
+impl<K: Key, V: Clone> Tree<K, V> {
+
+    /// Create a new tree
+    #[builder]
+    fn new(nodes: Option<SlotMap<K, Node<K, V>>>, root_nodes: Option<Vec<K>>) -> Self {
+        match (nodes, root_nodes) {
+            (Some(nodes), Some(root_nodes)) => Tree {
+                nodes,
+                children: Some(root_nodes)
+            },
+            _ => Tree {
+                nodes: SlotMap::with_key(),
+                children: Vec::new()
+            }}
+        }
     }
 
-    /// Inserts a branch and returns a key
-    fn insert(&mut self, value: &Node<K, B, L>) -> &K {
-        &self.0.insert(value.clone())
+    /// Given a node, insert it
+    fn insert(&mut self, value: Node<K, V>) -> K {
+        match Some(self.root_node) {
+            Some(root_node) => self.nodes.insert(value) // TODO: Insert at root_node
+            None => {
+                let key = self.nodes.insert(value);
+                self.root_node = key;
+            }
+        }
+
     }
-    // fn insert_branch_with_key(&mut self, branch: &B)
+    fn get(&self, key: K) -> Option<&Node<K, V>> {
+        self.nodes.get(key)
+    }
+    fn get_mut(&mut self, key: K) -> Option<&mut Node<K, V>> {
+        self.nodes.get_mut(key)
+    }
     /// Moves a node with key K to a given parent and index
-    fn move(&mut self, key: K, parent_key: K, index: u32);
+    fn move(&mut self, key: K, parent_key: K, index: u32) {
+        let node_option = self.get(key);
+        let parent_option = self.get_mut(parent_key);
+        match node {
+            Some(node) =>
+        }
+    }
+    // get children
+
 }
 
 // new_key_type! {
@@ -91,25 +126,25 @@ impl<K: Key, B: Clone, L: Clone> Tree<K, B, L> {
 mod tests {
     use super::*;
 
-    #[test]
-    /// Tests methods related to Nodes.
-    fn test_node() {
-        type TestNode = Node<u32, String, char>;
+    // #[test]
+    // /// Tests methods related to Nodes.
+    // fn test_node() {
+    //     type TestNode = Node<u32, String, char>;
 
-        // Create a basic hierarchy
-        // parent_node
-        //   - key for child_node
-        //   - key for child_node2
-        let child_node: TestNode = Node::builder().key(0).leaf('a').build();
-        let child_node2: TestNode = Node::builder().key(1).leaf('b').build();
-        let parent_node: TestNode = Node::builder()
-            .key(2)
-            .branch("directory1".into())
-            .children(vec![0, 1])
-            .build();
+    //     // Create a basic hierarchy
+    //     // parent_node
+    //     //   - key for child_node
+    //     //   - key for child_node2
+    //     let child_node: TestNode = Node::builder().key(0).leaf('a').build();
+    //     let child_node2: TestNode = Node::builder().key(1).leaf('b').build();
+    //     let parent_node: TestNode = Node::builder()
+    //         .key(2)
+    //         .branch("directory1".into())
+    //         .children(vec![0, 1])
+    //         .build();
 
-        assert_eq!(parent_node.len(), 2);
-    }
+    //     assert_eq!(parent_node.len(), 2);
+    // }
     #[test]
     /// Tests serialisation and deserialisation into a format compatible for a persistent storage medium.
     fn test_node_serialisation() {}

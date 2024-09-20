3c39a594fe7dbaa882ab8134308daa8177e7a378
Luke Martin <44519853+lkdm@users.noreply.github.com>
2024-09-20 17:29:10 +1000

    feat: create directory tree struct.

--- src/lib.rs
+++ src/lib.rs
@@ -26,14 +26,31 @@ enum NoteNode {
 type NoteId = u32;
 type DraftId = u32;
 
-/// A project defines the
-struct Project {
-    draft: DirectoryTree<DraftId, DraftNode>,
-    notes: DirectoryTree<NoteId, NoteNode>,
-}
+use std::{
+    cell::RefCell,
+    sync::{Arc, Weak},
+};
 
 // Make a tree with indexmap as backing storage
 use indexmap::{map::*, Equivalent};
+
+// TODO: IndexMap is one-dimensional.
+
+struct Project {
+    drafts: DirectoryTree<DraftId, DraftNode>,
+    notes: DirectoryTree<NoteId, NoteNode>,
+}
+
+enum Node<K: Equivalent<K>, T> {
+    Leaf {
+        inner: Arc<T>,
+    },
+    Branch {
+        inner: Arc<T>,
+        children: IndexMap<K, Node<K, T>>,
+        parent: Option<Weak<RefCell<Node<K, T>>>>,
+    },
+}
 
 /// DirectoryTree
 ///
@@ -45,7 +62,7 @@ use indexmap::{map::*, Equivalent};
 /// Index map is a hash table where the iteration order of key-value pairs is independent of the hash values of the keys.
 ///
 /// Keys must impl Equivalent
-struct DirectoryTree<K: Equivalent<K>, V>(IndexMap<K, V>);
+struct DirectoryTree<K: Equivalent<K>, T>(IndexMap<K, Node<K, T>>);
 
 impl<K: Equivalent<K>, V> DirectoryTree<K, V> {
     /// Creates a new empty DirectoryTree.
